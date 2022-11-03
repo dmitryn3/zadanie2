81bf1cea30392197a43bd7c53aa9b5f0711c2778
dmitryn3 <dmitryn3@yandex.ru>
2022-11-03 14:05:09 +0300

    Srednee

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         int[] sales = {5, 8, 10};
         SalesManager manager = new SalesManager(sales);
         System.out.println("Max: " + manager.max());
+        System.out.println("Среднее: " + manager.srednee());
 
     }
 }
